6f58e6d7b9e967da706f689da74703ac193bf856
erikhsaragih <erikhsaragih@users.noreply.github.com>
2023-11-06 08:49:41 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -16,7 +16,7 @@ all_df.sort_values(by="order_approved_at", inplace=True)
 all_df.reset_index(inplace=True)
 
 # Geolocation Dataset
-geolocation = pd.read_csv('geolocation.csv', on_bad_lines='skip')
+geolocation = pd.read_csv('geolocation.csv')
 data = geolocation.drop_duplicates(subset='customer_unique_id')
 
 for col in datetime_cols:
