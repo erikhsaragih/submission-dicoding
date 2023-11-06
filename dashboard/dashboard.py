b70a235540163cb01d88fb0145b316ebe79a526b
erikhsaragih <erikhsaragih@users.noreply.github.com>
2023-11-06 08:37:17 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -11,12 +11,12 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Dataset
 datetime_cols = ["order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date", "order_purchase_timestamp", "shipping_limit_date"]
-all_df = pd.read_csv("https://github.com/erikhsaragih/submission-dicoding/blob/main/dashboard/all_data.csv", on_bad_lines='skip')
+all_df = pd.read_csv("all_data.csv", on_bad_lines='skip')
 all_df.sort_values(by="order_approved_at", inplace=True)
 all_df.reset_index(inplace=True)
 
 # Geolocation Dataset
-geolocation = pd.read_csv('https://github.com/erikhsaragih/submission-dicoding/blob/main/dashboard/geolocation.csv', on_bad_lines='skip')
+geolocation = pd.read_csv('geolocation.csv', on_bad_lines='skip')
 data = geolocation.drop_duplicates(subset='customer_unique_id')
 
 for col in datetime_cols:
